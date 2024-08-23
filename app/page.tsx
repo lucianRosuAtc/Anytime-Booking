e9cdacb42b5d50b6cadaa2a4fdcf69c357cc5508
Lucian Rosu <lucian.rosu.atc@gmail.com>
2024-08-23 15:43:49 +0100

    mobile customisation

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Form from "@/components/Form";
 export default function Home() {
   return (
     <div>
-      <h1>Anytime Booking: Lucian Rosu&apos;s Test Project</h1>
+      <h1 className="mx-3">Anytime Booking: Lucian Rosu&apos;s Test Project</h1>
       <Form />
     </div>
   );
